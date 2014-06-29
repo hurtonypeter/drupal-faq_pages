be4035e95bfb4fd2558e297c57bf38efbe883219
Hurtony Péter <hurtonypeter@gmail.com>
2014-06-29 13:35:35 +0200

    routing, local_tasks and skeleton

---
+++ lib/Drupal/faq_pages/Controller/FaqPagesController.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\faq_pages\Controller\FaqPagesController.
+ */
+
+namespace Drupal\faq_pages\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Controller routines for FAQ Pages routes.
+ */
+class FaqPagesController extends ControllerBase {
+  
+  public function listPages() {
+    
+  }
+  
+  public function editPage() {
+    
+  }
+  
+  public function viewPage() {
+    
+  }
+  
+}
