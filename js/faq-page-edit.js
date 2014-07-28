b54d3b4bece517bbce7ae61a96a986cc28ada768
hurtonypeter <hurtonypeter@gmail.com>
2014-07-28 03:57:22 +0200

    put the whole model into one js object

--- js/faq-page-edit.js
+++ js/faq-page-edit.js
@@ -26,17 +26,19 @@
       return [value];
     });
 
-    $scope.title = "muhahhahaha";
-    $scope.url = "/sdf";
-    $scope.description = "little descrp";
-    $scope.blocks = [
-      {id: 1, name: 'blokk1', description: 'blockdecrtiption1', terms: []},
-      {id: 2, name: 'blocck2', description: 'blockdecrtiption2', terms: []},
-      {id: 3, name: 'blokk3', description: 'blockdecrtiption3', terms: []}
-    ];
+    $scope.model = {
+      title: 'muhahhahaha',
+      url: '/sdf',
+      description: 'little descrp',
+      blocks: [
+        {id: 1, name: 'blokk1', description: 'blockdecrtiption1', terms: []},
+        {id: 2, name: 'blocck2', description: 'blockdecrtiption2', terms: []},
+        {id: 3, name: 'blokk3', description: 'blockdecrtiption3', terms: []}
+      ]
+    };
 
     $scope.newBlock = function() {
-      $scope.blocks.push({
+      $scope.model.blocks.push({
         id: null,
         name: '',
         description: '',
@@ -45,41 +47,41 @@
     };
 
     $scope.deleteBlock = function(id) {
-      $scope.blocks.splice(id, 1);
+      $scope.model.blocks.splice(id, 1);
     };
 
     $scope.blockUp = function(block) {
-      var temp = $scope.blocks[block - 1];
-      $scope.blocks[block - 1] = $scope.blocks[block];
-      $scope.blocks[block] = temp;
+      var temp = $scope.model.blocks[block - 1];
+      $scope.model.blocks[block - 1] = $scope.model.blocks[block];
+      $scope.model.blocks[block] = temp;
     };
 
     $scope.blockDown = function(block) {
-      var temp = $scope.blocks[block + 1];
-      $scope.blocks[block + 1] = $scope.blocks[block];
-      $scope.blocks[block] = temp;
+      var temp = $scope.model.blocks[block + 1];
+      $scope.model.blocks[block + 1] = $scope.model.blocks[block];
+      $scope.model.blocks[block] = temp;
     };
 
     $scope.termUp = function(term, block) {
-      var temp = $scope.blocks[block].terms[term - 1];
-      $scope.blocks[block].terms[term - 1] = $scope.blocks[block].terms[term];
-      $scope.blocks[block].terms[term] = temp;
+      var temp = $scope.model.blocks[block].terms[term - 1];
+      $scope.model.blocks[block].terms[term - 1] = $scope.model.blocks[block].terms[term];
+      $scope.model.blocks[block].terms[term] = temp;
     };
 
     $scope.termDown = function(term, block) {
-      var temp = $scope.blocks[block].terms[term + 1];
-      $scope.blocks[block].terms[term + 1] = $scope.blocks[block].terms[term];
-      $scope.blocks[block].terms[term] = temp;
+      var temp = $scope.model.blocks[block].terms[term + 1];
+      $scope.model.blocks[block].terms[term + 1] = $scope.model.blocks[block].terms[term];
+      $scope.model.blocks[block].terms[term] = temp;
     };
-    
+
     $scope.deleteTerm = function(term, block) {
-      $scope.blocks[block].terms.splice(term, 1);
+      $scope.model.blocks[block].terms.splice(term, 1);
     };
 
     $scope.hideMe = function(terms) {
       return terms.length == 0;
     };
-    
+
   });
 
 })(jQuery);
